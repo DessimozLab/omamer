7f53def69640c655f4ebc6d0d5a55effb512b02b
VictorRossier <victor.rossier@unil.ch>
2020-08-10 11:07:26 +0200

    detail

--- omamer/_run_validation.py
+++ omamer/_run_validation.py
@@ -125,7 +125,7 @@ subfamily_query_sp2thresholds = {
 focal_taxon = "Metazoa"
 bin_num = 1
 val_mode = 'golike' 
-neg_root_taxon = (None if sys.argv[6] == 'Random') else sys.argv[6]
+neg_root_taxon = None if (sys.argv[6] == 'Random') else sys.argv[6]
 chunksize = int(sys.argv[7])
 
 run_validation_pipeline(

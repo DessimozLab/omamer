31b3cdd250ddf8a7b0cf5de640c6e43590ecc2c3
VictorRossier <victor.rossier@unil.ch>
2021-01-27 18:25:23 +0100

    update

--- omamer/_runners_axiom.py
+++ omamer/_runners_axiom.py
@@ -1,3 +1,24 @@
+'''
+    OMAmer - tree-driven and alignment-free protein assignment to sub-families
+
+    (C) 2019-2020 Victor Rossier <victor.rossier@unil.ch> and
+                  Alex Warwick Vesztrocy <alex@warwickvesztrocy.co.uk>
+
+    This file is part of OMAmer.
+
+    OMAmer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OMAmer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with OMAmer. If not, see <http://www.gnu.org/licenses/>.
+'''
 import sys
 omamer_path = sys.argv[1]
 sys.path.insert(0, omamer_path)
@@ -19,12 +40,6 @@ import shutil
 import numpy as np
 from tqdm import tqdm
 
-'''
-General functions to run OMAmer on axiom (e.g. from notebook).
-
-TO DO:
- - check search_validate step
-'''
 
 def is_complete(fn, path):
     cf = '{}COMPLETE.txt'.format(path)

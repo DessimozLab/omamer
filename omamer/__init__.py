54877ca586ffcb4b97019fc5712875e4de1925e7
Alex Warwick Vesztrocy <alex@warwickvesztrocy.co.uk>
2024-03-28 18:50:51 +0100

    bump 2.0.4.dev4:wq

--- omamer/__init__.py
+++ omamer/__init__.py
@@ -23,7 +23,7 @@
 from datetime import date
 
 __packagename__ = "omamer"
-__version__ = "2.0.4.dev3"
+__version__ = "2.0.4.dev4"
 __copyright__ = "(C) 2019-{:d} Victor Rossier <victor.rossier@unil.ch> and Alex Warwick Vesztrocy <alex@warwickvesztrocy.co.uk>".format(
     date.today().year
 )

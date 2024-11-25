0bbb6e9edd06f9a938b3a8416b6088cb1614ef2e
Nikolai Romashchenko <nikolay.romashchenko@gmail.com>
2024-11-25 10:38:42 +0100

    Updated license headers

--- omamer/_clock.py
+++ omamer/_clock.py
@@ -1,3 +1,27 @@
+"""
+    OMAmer - tree-driven and alignment-free protein assignment to sub-families
+
+    (C) 2024 Nikolai Romashchenko <nikolai.romashchenko@unil.ch>
+    (C) 2022-2023 Alex Warwick Vesztrocy <alex.warwickvesztrocy@unil.ch>
+    (C) 2019-2021 Victor Rossier <victor.rossier@unil.ch> and
+                  Alex Warwick Vesztrocy <alex@warwickvesztrocy.co.uk>
+
+    This file is part of OMAmer.
+
+    OMAmer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OMAmer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with OMAmer. If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import ctypes
 
 # Access the _PyTime_AsSecondsDouble and _PyTime_GetSystemClock functions from pythonapi

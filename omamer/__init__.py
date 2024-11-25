15070a99ae801ced1da7eba368a7bd9b9e2588a3
Nikolai Romashchenko <nikolay.romashchenko@gmail.com>
2024-11-25 10:48:32 +0100

    Bump version: 2.0.4 → 2.1.0

--- omamer/__init__.py
+++ omamer/__init__.py
@@ -24,7 +24,7 @@
 from datetime import date
 
 __packagename__ = "omamer"
-__version__ = "2.0.5"
+__version__ = "2.1.0"
 __copyright__ = "(C) 2019-{:d} Victor Rossier <victor.rossier@unil.ch> and Alex Warwick Vesztrocy <alex@warwickvesztrocy.co.uk> and Nikolai Romashchenko <nikolai.romashchenko@unil.ch>".format(
     date.today().year
 )
